9b15c4eb735ac08212f403d396cf685c9f383e1c
Mohamed-Kamal0 <149805518+Mohamed-Kamal0@users.noreply.github.com>
2024-04-08 14:35:07 +0200

    s

--- Source.cpp
+++ Source.cpp
@@ -9,18 +9,33 @@
 #include<string>
 using namespace std;
 
-void inputfn(int& N, int& Es, int& Et, int& Eg, int& As, int& Am, int& Ad, int& Prob) {
+void inputfn(int& N, int& Es, int& Et, int& Eg, int& As, int& Am, int& Ad, int& Prob,
+	int& epower1, int& epower2, int& ehealth1, int& ehealth2, int& eattackcap1, int& eattackcap2,
+	int& apower1, int& apower2, int& ahealth1, int& ahealth2, int& aattackcap1, int& aattackcap2) {
 	ifstream f("in1.text");
-	f >> N>>Prob>>Es>>Et>>Eg>>As>>Am>>Ad;
+	f >> N>>Prob>>Es>>Et>>Eg>>As>>Am>>Ad>> epower1>> epower2>> ehealth1>> ehealth2>> eattackcap1
+		>> eattackcap2>> apower1>> apower2>> ahealth1>> ahealth2>> aattackcap1>> aattackcap2;
+	ehealth2 = -1 * ehealth2;
+	ahealth2 = -1 * ahealth2;
+	epower2 = -1 * epower2;
+	apower2 = -1 * apower2;
+	eattackcap2 = -1 * eattackcap2;
+	aattackcap2 = -1 * aattackcap2;
 
 }
 int main() {
 
 	
 	
-	int n, es, et, eg, as, am, ad, prob;
-	inputfn(n,es, et, eg, as, am, ad, prob);
-	cout << n <<endl<< prob << endl << es << endl << et << endl << eg << endl << as << endl << am << endl << ad << endl << endl;
+	int n, es, et, eg, as, am, ad, prob,epower1,epower2,ehealth1,ehealth2,eattackcap1,eattackcap2,
+		apower1, apower2, ahealth1, ahealth2, aattackcap1, aattackcap2;
+	inputfn(n,es, et, eg, as, am, ad, prob,epower1, epower2, ehealth1, ehealth2,
+		eattackcap1, eattackcap2,
+		apower1, apower2, ahealth1, ahealth2, aattackcap1, aattackcap2);
+	cout << n <<endl<< prob << endl << es << endl << et << endl << eg << endl << as << endl << am << endl << ad << endl << epower1
+		<< endl << epower2<< endl << ehealth1 << endl << ehealth2<< endl << eattackcap1<< endl << eattackcap2
+		<< endl << apower1<< endl << apower2 <<  endl << ahealth1 << endl << ahealth2 << endl << aattackcap1 << endl << 
+		aattackcap2 <<endl << endl;
 	/*
 	AlineDroneList a;
 	unit* u1=new AlienDrone ;
