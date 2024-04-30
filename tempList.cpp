3404a6b402ae0def31cbb11b196000814fea5047
Mohamed-Kamal0 <149805518+Mohamed-Kamal0@users.noreply.github.com>
2024-04-30 14:10:58 +0300

    adding Tempclass

---
+++ tempList.cpp
@@ -0,0 +1,54 @@
+#include "tempList.h"
+
+tempList::tempList() :count(0)
+{
+}
+
+tempList::~tempList()
+{
+	unit* A;
+	while (list.dequeue(A)) {
+		delete A;
+		A = nullptr;
+	}
+}
+
+bool tempList::insert(unit* killedUnit)
+{
+	count++;
+	return list.enqueue(killedUnit);
+}
+
+bool tempList::remove(unit*& tempunit)
+{
+		if (list.dequeue(tempunit)) {
+			count--;
+			return 1;
+		}
+		return 0;
+}
+
+void tempList::print()
+{
+	LinkedQueue<unit*> temp;
+	unit* tempUnit;
+	cout << "\n============== Temp Units ==============" << endl;
+	cout << count << " units [";
+	while (!list.isEmpty())
+	{
+		list.dequeue(tempUnit);
+		cout << tempUnit->get_id();
+		temp.enqueue(tempUnit);
+		if (!list.isEmpty())
+		{
+			cout << ", ";
+		}
+	}
+	cout << "]" << endl;
+
+	while (!temp.isEmpty())
+	{
+		temp.dequeue(tempUnit);
+		list.enqueue(tempUnit);
+	}
+}
